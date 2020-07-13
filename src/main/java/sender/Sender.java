1f932adbf9c286b375f8acffb5af15b54e9a98d2
juan.etcheverry <juan.etcheverry@neoris.com>
2020-07-13 19:07:01 -0300

    Agregar emisor de mensaje para cola de tareas

--- src/main/java/sender/Sender.java
+++ src/main/java/sender/Sender.java
@@ -9,17 +9,17 @@ import java.nio.charset.StandardCharsets;
 public class Sender {
 	  private final static String QUEUE_NAME = "hello";
 
-
 	    public static void main(String[] argv) throws Exception {
 	        ConnectionFactory factory = new ConnectionFactory();
 	        factory.setHost("localhost");
+	        
 	        try (Connection connection = factory.newConnection();
-	             Channel channel = connection.createChannel()) {
-	            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-	            String message = "Hello World!";
-	            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
-	            System.out.println(" [x] Sent '" + message + "'");
-	        }
+		            Channel channel = connection.createChannel()) {
+		            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		            String message = "Hello World!";
+		            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+		            System.out.println(" [x] Sent '" + message + "'");
+		        }
 	    }
 
 }
